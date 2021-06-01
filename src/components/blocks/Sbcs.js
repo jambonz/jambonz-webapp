3e629c9ab40e65e9b767403968336a9a8d638943
MikeMaven <mikemavendeveloper@gmail.com>
2021-06-01 14:09:36 -0400

    fix Sbcs

--- src/components/blocks/Sbcs.js
+++ src/components/blocks/Sbcs.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { NotificationDispatchContext } from '../../contexts/NotificationContext';
+import { ServiceProviderValueContext } from '../../contexts/ServiceProviderContext';
 
 const Container = styled.div`
   margin-top: 0.25rem;
@@ -20,6 +21,7 @@ const Container = styled.div`
 const Sbcs = props => {
   let history = useHistory();
   const dispatch = useContext(NotificationDispatchContext);
+  const currentServiceProvider = useContext(ServiceProviderValueContext);
   const [ sbcs, setSbcs ] = useState('');
   useEffect(() => {
     const getAPIData = async () => {
@@ -36,7 +38,7 @@ const Sbcs = props => {
         const sbcResults = await axios({
           method: 'get',
           baseURL: process.env.REACT_APP_API_BASE_URL,
-          url: '/Sbcs',
+          url: `/Sbcs?service_provider_sid=${currentServiceProvider}`,
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
