c7a688edfe71ae93fa283facff421f73f484a4e4
Dan Jenkins <dan@nimblea.pe>
2021-09-25 23:57:02 +0100

    beginnings of a UI for setting up subspace on a jambonz account

--- src/components/pages/internal/AccountsList.js
+++ src/components/pages/internal/AccountsList.js
@@ -43,6 +43,7 @@ const AccountsList = () => {
         sip_realm:   a.sip_realm,
         url_reg:     a.registration_hook && a.registration_hook.url,
         url_queue:   a.queue_event_hook && a.queue_event_hook.url,
+        subspace_enabled: a.subspace_sip_teleport_id ? 'Enabled' : ''
       }));
       return(simplifiedAccounts);
     } catch (err) {
@@ -221,7 +222,7 @@ const AccountsList = () => {
           { header: 'SIP Realm',            key: 'sip_realm' },
           { header: 'Registration Webhook', key: 'url_reg'       },
           { header: 'Queue Event Webhook',  key: 'url_queue'       },
-          { header: 'Subspace', key: 'subspace_enabled' },
+          { header: 'Subspace',             key: 'subspace_enabled' },
         ]}
         formatContentToDelete={formatAccountToDelete}
         deleteContent={deleteAccount}
