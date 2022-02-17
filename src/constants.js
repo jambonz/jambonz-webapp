ddad55b92f824ad0a8bf0a96152bbf5da1ad3343
Andrew Karp <drewkarp@gmail.com>
2022-02-17 15:29:23 +0200

    updated constants for window.jambonz

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 let { NODE_ENV, REACT_APP_API_BASE_URL } = process.env;
-export const APP_API_BASE_URL = (NODE_ENV === "production")
-  ? window.JAMBONZ.APP_API_BASE_URL 
+
+export const APP_API_BASE_URL = (window.JAMBONZ && NODE_ENV === "production")
+  ? window.JAMBONZ.APP_API_BASE_URL
   : REACT_APP_API_BASE_URL;
