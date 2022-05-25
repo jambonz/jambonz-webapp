37f539decb760b1ba768e5f7976253b5676516ab
khang <khanghugo123@gmail.com>
2022-05-25 10:25:38 -0400

    update azure region list

--- src/data/MicrosoftAzureRegions.js
+++ src/data/MicrosoftAzureRegions.js
@@ -1,96 +1,94 @@
 const regions = [
-  {
-    name: 'Asia (East)',
-    value: 'eastasia'
-  },
-  {
-    name: 'Asia (Southeast)',
-    value: 'southeastasia'
-  },
-  {
-    name: 'Australia (East)',
-    value: 'australiaeast'
-  },
-  {
-    name: 'Brazil (South)',
-    value: 'brazilsouth'
-  },
-  {
-    name: 'Canada (Central)',
-    value: 'canadacentral'
-  },
-  {
-    name: 'Europe (North)',
-    value: 'northeurope'
-  },
-  {
-    name: 'Europe (West)',
-    value: 'westeurope'
-  },
-  {
-    name: 'France (Central)',
-    value: 'francecentral'
-  },
-  {
-    name: 'Switzerland (North)',
-    value: 'switzerlandnorth'
-  },
-  {
-    name: 'India (Central)',
-    value: 'centralindia'
-  },
-  {
-    name: 'Japan (West)',
-    value: 'japanwest'
-  },
-  {
-    name: 'Japan (East)',
-    value: 'japaneast'
-  },
-  {
-    name: 'Korea (Central)',
-    value: 'koreacentral'
-  },
-  {
-    name: 'South Africa (North)',
-    value: 'southafricanorth'
-  },
-  {
-    name: 'UK (South)',
-    value: 'uksouth'
-  },
-  {
-    name: 'US (Cental)',
-    value: 'centralus'
-  },
-  {
-    name: 'US (West Central)',
-    value: 'westcentralus'
-  },
-  {
-    name: 'US (East)',
-    value: 'eastus'
-  },
-  {
-    name: 'US (East 2)',
-    value: 'eastus2'
-  },
-  {
-    name: 'US (North Central)',
-    value: 'northcentralus'
-  },
-  {
-    name: 'US (South Central)',
-    value: 'southcentralus'
-  },
-  {
-    name: 'US (West)',
-    value: 'westus'
-  },
-  {
-    name: 'US (West 2)',
-    value: 'westus2'
-  },
+  { 
+    name: 'Africa North', 
+    value: 'southafricanorth' },
+  { 
+    name: 'East Asia', 
+    value: 'eastasia' },
+  { 
+    name: 'Southeast Asia', 
+    value: 'southeastasia' },
+  { 
+    name: 'Australia East', 
+    value: 'australiaeast' },
+  { 
+    name: 'Central India', 
+    value: 'centralindia' },
+  { 
+    name: 'Japan East', 
+    value: 'japaneast' },
+  { 
+    name: 'Japan West', 
+    value: 'japanwest' },
+  { 
+    name: 'Korea Central', 
+    value: 'koreacentral' },
+  { 
+    name: 'Canada Central', 
+    value: 'canadacentral' },
+  { 
+    name: 'North Europe', 
+    value: 'northeurope' },
+  { 
+    name: 'West Europe', 
+    value: 'westeurope' },
+  { 
+    name: 'France Central', 
+    value: 'francecentral' },
+  { 
+    name: 'Germany West Central', 
+    value: 'germanywestcentral' },
+  { 
+    name: 'Norway East', 
+    value: 'norwayeast' },
+  { 
+    name: 'Switzerland North', 
+    value: 'switzerlandnorth' },
+  { 
+    name: 'Switzerland West', 
+    value: 'switzerlandwest' },
+  { 
+    name: 'UK South', 
+    value: 'uksouth' },
+  { 
+    name: 'UAE North', 
+    value: 'uaenorth' },
+  { 
+    name: 'Brazil South', 
+    value: 'brazilsouth' },
+  { 
+    name: 'Central US', 
+    value: 'centralus' },
+  { 
+    name: 'East US', 
+    value: 'eastus' },
+  { 
+    name: 'East US 2', 
+    value: 'eastus2' },
+  { 
+    name: 'North Central US', 
+    value: 'northcentralus' },
+  { 
+    name: 'South Central US', 
+    value: 'southcentralus' },
+  { 
+    name: 'US Gov Arizona', 
+    value: 'usgovarizona' },
+  { 
+    name: 'US Gov Virginia', 
+    value: 'usgovvirginia' },
+  { 
+    name: 'West Central US', 
+    value: 'westcentralus' },
+  { 
+    name: 'West US', 
+    value: 'westus' },
+  { 
+    name: 'West US 2', 
+    value: 'westus2' },
+  { 
+    name: 'West US 3', 
+    value: 'westus3' }
 ];
 
 export default regions;
