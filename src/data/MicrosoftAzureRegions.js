de77853333f5c2cbac24b5779ca0bb4b47505070
khang <khanghugo123@gmail.com>
2022-05-25 10:54:51 -0400

    sorting and prettifying

--- src/data/MicrosoftAzureRegions.js
+++ src/data/MicrosoftAzureRegions.js
@@ -1,122 +1,122 @@
 const regions = [
   { 
-    name: 'Africa North', 
+    name: 'Africa - North (southafricanorth)', 
     value: 'southafricanorth' 
   },
   { 
-    name: 'East Asia', 
-    value: 'eastasia' 
-  },
-  { 
-    name: 'Southeast Asia', 
-    value: 'southeastasia' 
-  },
-  { 
-    name: 'Australia East', 
+    name: 'Australia - East (australiaeast)', 
     value: 'australiaeast' 
   },
   { 
-    name: 'India Central', 
-    value: 'centralindia' 
-  },
-  { 
-    name: 'Japan East', 
-    value: 'japaneast' 
-  },
-  { 
-    name: 'Japan West', 
-    value: 'japanwest' 
-  },
-  { 
-    name: 'Korea Central', 
-    value: 'koreacentral' 
-  },
-  { 
-    name: 'Canada Central', 
-    value: 'canadacentral' 
-  },
-  { 
-    name: 'Europe North', 
-    value: 'northeurope' 
-  },
-  { 
-    name: 'Europe West', 
-    value: 'westeurope' 
-  },
-  { 
-    name: 'France Central', 
-    value: 'francecentral' 
-  },
-  { 
-    name: 'Germany West Central', 
-    value: 'germanywestcentral' 
-  },
-  { 
-    name: 'Norway East', 
-    value: 'norwayeast' 
-  },
-  { 
-    name: 'Switzerland North', 
-    value: 'switzerlandnorth' 
-  },
-  { 
-    name: 'Switzerland West', 
-    value: 'switzerlandwest' 
-  },
-  { 
-    name: 'UK South', 
-    value: 'uksouth' 
-  },
-  { 
-    name: 'UAE North', 
-    value: 'uaenorth' 
-  },
-  { 
-    name: 'Brazil South', 
+    name: 'Brazil - South (brazilsouth)', 
     value: 'brazilsouth' 
   },
   { 
-    name: 'US Central', 
+    name: 'Canada - Central (canadacentral)', 
+    value: 'canadacentral' 
+  },
+  { 
+    name: 'East Asia (eastasia)', 
+    value: 'eastasia' 
+  },
+  { 
+    name: 'Europe - North (northeurope)', 
+    value: 'northeurope' 
+  },
+  { 
+    name: 'Europe - West (westeurope)', 
+    value: 'westeurope' 
+  },
+  { 
+    name: 'France - Central (francecentral)', 
+    value: 'francecentral' 
+  },
+  { 
+    name: 'Germany - West Central (germanywestcentral)', 
+    value: 'germanywestcentral' 
+  },
+  { 
+    name: 'India - Central (centralindia)', 
+    value: 'centralindia' 
+  },
+  { 
+    name: 'Japan - East (japaneast)', 
+    value: 'japaneast' 
+  },
+  { 
+    name: 'Japan - West (japanwest)', 
+    value: 'japanwest' 
+  },
+  { 
+    name: 'Korea - Central (koreacentral)', 
+    value: 'koreacentral' 
+  },
+  { 
+    name: 'Norway - East (norwayeast)', 
+    value: 'norwayeast' 
+  },
+  { 
+    name: 'Southeast Asia (southeastasia)', 
+    value: 'southeastasia' 
+  },
+  { 
+    name: 'Switzerland - North (switzerlandnorth)', 
+    value: 'switzerlandnorth' 
+  },
+  { 
+    name: 'Switzerland - West (switzerlandwest)', 
+    value: 'switzerlandwest' 
+  },
+  { 
+    name: 'UAE - North (uaenorth)', 
+    value: 'uaenorth' 
+  },
+  { 
+    name: 'UK - South (uksouth)', 
+    value: 'uksouth' 
+  },
+  { 
+    name: 'US - Central (centralus)', 
     value: 'centralus' 
   },
   { 
-    name: 'US East', 
+    name: 'US - East (eastus)', 
     value: 'eastus' 
   },
   { 
-    name: 'US East 2', 
+    name: 'US - East 2 (eastus2)', 
     value: 'eastus2' 
   },
   { 
-    name: 'US North Central', 
-    value: 'northcentralus' 
-  },
-  { 
-    name: 'US South Central', 
-    value: 'southcentralus' 
-  },
-  { 
-    name: 'US Gov Arizona', 
+    name: 'US - Gov Arizona (usgovarizona)', 
     value: 'usgovarizona' 
   },
   { 
-    name: 'US Gov Virginia', 
+    name: 'US - Gov Virginia (usgovvirginia)', 
     value: 'usgovvirginia' 
   },
   { 
-    name: 'US West Central', 
+    name: 'US - North Central (northcentralus)', 
+    value: 'northcentralus' 
+  },
+  { 
+    name: 'US - South Central (southcentralus)', 
+    value: 'southcentralus' 
+  },
+  { 
+    name: 'US - West Central (westcentralus)', 
     value: 'westcentralus' 
   },
   { 
-    name: 'US West', 
+    name: 'US - West (westus)', 
     value: 'westus' 
   },
   { 
-    name: 'US West 2', 
+    name: 'US - West 2 (westus2)', 
     value: 'westus2' 
   },
   { 
-    name: 'US West 3', 
+    name: 'US - West 3 (westus3)', 
     value: 'westus3' 
   }
 ];
