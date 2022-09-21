1c89fb0e1e38f31fb003588ec296b6056f8b76c5
Quan Luu <quan.luuhoang8@gmail.com>
2022-09-21 21:25:57 +0700

    fix: review comment

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,11 @@ const { REACT_APP_API_BASE_URL } = process.env;
 export const APP_API_BASE_URL = (window.JAMBONZ) ? window.JAMBONZ.APP_API_BASE_URL : REACT_APP_API_BASE_URL;
 export const LIMITS = [
     // {
-    //   label: "Max registered devices",
+    //   label: "Max registered devices (0=unlimited)",
     //   category: "device",
     // },
     // {
-    //   label: "Max api calls per minute",
+    //   label: "Max api calls per minute (0=unlimited)",
     //   category: "api_rate",
     // },
     {
