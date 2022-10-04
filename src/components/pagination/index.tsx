18f531c478e35ebb1c50b56248311956028f66b9
kitajchuk <bk@kitajchuk.com>
2022-10-04 07:28:53 -0700

    Pagination optimizations

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { Icon } from "jambonz-ui";
 
 import { Icons } from "../icons";
@@ -24,9 +24,84 @@ export const Pagination = ({
   jumpNum = 3,
   showMin = 4,
 }: PaginationProps) => {
-  const handleSetPageNumber = (num: number) => {
-    setPageNumber(Math.max(1, Math.min(maxPageNumber, num)));
-  };
+  const memoizedPages = useMemo(() => {
+    return Array(maxPageNumber).fill(0);
+  }, [maxPageNumber]);
+
+  const handleSetPageNumber = useCallback(
+    (num: number) => {
+      setPageNumber(Math.max(1, Math.min(maxPageNumber, num)));
+    },
+    [maxPageNumber, setPageNumber]
+  );
+
+  const handleNumberMapping = useCallback(
+    (_: number, index: number) => {
+      const num = index + 1;
+
+      if (
+        pageNumber === num ||
+        num === 1 ||
+        num === maxPageNumber ||
+        (pageNumber > num && pageNumber <= num + nextTo) ||
+        (pageNumber < num && pageNumber >= num - nextTo) ||
+        (pageNumber <= showMin && num <= showMin) ||
+        (maxPageNumber - pageNumber + 1 <= showMin &&
+          maxPageNumber - num + 1 <= showMin)
+      ) {
+        return (
+          <button
+            type="button"
+            onClick={() => handleSetPageNumber(num)}
+            key={num}
+          >
+            {num === pageNumber ? (
+              <Icon>{num}</Icon>
+            ) : (
+              <Icon subStyle="grey">{num}</Icon>
+            )}
+          </button>
+        );
+      }
+
+      if (
+        pageNumber >= showMin
+          ? num === pageNumber + nextTo + 1
+          : num === showMin + 1
+      ) {
+        return (
+          <button
+            key="jump_right"
+            type="button"
+            onClick={() => handleSetPageNumber(pageNumber + jumpNum)}
+          >
+            <Icon subStyle="grey">
+              <Icons.MoreHorizontal />
+            </Icon>
+          </button>
+        );
+      }
+
+      if (
+        maxPageNumber - pageNumber + 1 >= showMin
+          ? num === pageNumber - nextTo - 1
+          : maxPageNumber - num + 1 === showMin + 1
+      ) {
+        return (
+          <button
+            key="jump_left"
+            type="button"
+            onClick={() => handleSetPageNumber(pageNumber - jumpNum)}
+          >
+            <Icon subStyle="grey">
+              <Icons.MoreHorizontal />
+            </Icon>
+          </button>
+        );
+      }
+    },
+    [maxPageNumber, pageNumber]
+  );
 
   return (
     <div className="pagination">
@@ -39,72 +114,7 @@ export const Pagination = ({
           <Icons.ChevronLeft />
         </Icon>
       </button>
-      {Array(maxPageNumber)
-        .fill(0)
-        .map((_, index) => {
-          const num = index + 1;
-
-          if (
-            pageNumber === num ||
-            num === 1 ||
-            num === maxPageNumber ||
-            (pageNumber > num && pageNumber <= num + nextTo) ||
-            (pageNumber < num && pageNumber >= num - nextTo) ||
-            (pageNumber <= showMin && num <= showMin) ||
-            (maxPageNumber - pageNumber + 1 <= showMin &&
-              maxPageNumber - num + 1 <= showMin)
-          ) {
-            return (
-              <button
-                type="button"
-                onClick={() => handleSetPageNumber(num)}
-                key={num}
-              >
-                {num === pageNumber ? (
-                  <Icon>{num}</Icon>
-                ) : (
-                  <Icon subStyle="grey">{num}</Icon>
-                )}
-              </button>
-            );
-          }
-
-          if (
-            pageNumber >= showMin
-              ? num === pageNumber + nextTo + 1
-              : num === showMin + 1
-          ) {
-            return (
-              <button
-                key="jump_right"
-                type="button"
-                onClick={() => handleSetPageNumber(pageNumber + jumpNum)}
-              >
-                <Icon subStyle="grey">
-                  <Icons.MoreHorizontal />
-                </Icon>
-              </button>
-            );
-          }
-
-          if (
-            maxPageNumber - pageNumber + 1 >= showMin
-              ? num === pageNumber - nextTo - 1
-              : maxPageNumber - num + 1 === showMin + 1
-          ) {
-            return (
-              <button
-                key="jump_left"
-                type="button"
-                onClick={() => handleSetPageNumber(pageNumber - jumpNum)}
-              >
-                <Icon subStyle="grey">
-                  <Icons.MoreHorizontal />
-                </Icon>
-              </button>
-            );
-          }
-        })}
+      {memoizedPages.map(handleNumberMapping)}
       <button
         type="button"
         disabled={pageNumber === maxPageNumber}
