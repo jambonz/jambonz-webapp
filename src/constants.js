4535820828798005d0cf0955c17759ddf836b3a1
Dave Horton <daveh@beachdognet.com>
2022-11-27 15:29:57 -0500

    add support for tracking add'l account limits

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const { REACT_APP_API_BASE_URL } = process.env;
+const { REACT_APP_API_BASE_URL, REACT_APP_ENABLE_ACCOUNT_LIMITS_ALL } = process.env;
 export const APP_API_BASE_URL = (window.JAMBONZ) ? window.JAMBONZ.APP_API_BASE_URL : REACT_APP_API_BASE_URL;
 export const LIMITS = [
     // {
@@ -10,7 +10,22 @@ export const LIMITS = [
     //   category: "api_rate",
     // },
     {
-      label: "Max concurrent calls (0=unlimited)",
+      label: "Max calls",
       category: "voice_call_session",
-    },
+    }
   ];
+
+  if (REACT_APP_ENABLE_ACCOUNT_LIMITS_ALL) {
+    LIMITS.push({
+      label: "Licensed calls",
+      category: "voice_call_session_license",
+    });
+    LIMITS.push({
+      label: "Max minutes",
+      category: "voice_call_minutes",
+    });
+    LIMITS.push({
+      label: "Licensed minutes",
+      category: "voice_call_minutes_license",
+    });
+  }
