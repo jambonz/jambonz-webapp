9fe3469e5fed4bd29914a4275da780c39c96d598
eglehelms <e.helms@cognigy.com>
2022-12-01 19:26:49 +0100

    add ibm speech

---
+++ src/vendor/regions/ibm-regions.ts
@@ -0,0 +1,34 @@
+import type { Region } from "../types";
+
+export const regions: Region[] = [
+  {
+    name: "US South (Dallas)",
+    value: "us-south",
+  },
+  {
+    name: "US East (Washington)",
+    value: "us-east",
+  },
+  {
+    name: "EU DE (Frankfurt)",
+    value: "eu-de",
+  },
+  {
+    name: "EU GB (London)",
+    value: "eu-gb",
+  },
+  {
+    name: "AU South (Sydney)",
+    value: "au-syd",
+  },
+  {
+    name: "Japan (Tokyo)",
+    value: "jp-tok",
+  },
+  {
+    name: "South Korea (Seul)",
+    value: "kr-seo",
+  },
+];
+
+export default regions;
---
+++ src/vendor/speech-recognizer/ibm-speech-recognizer-lang.ts
@@ -0,0 +1,130 @@
+import type { Language } from "../types";
+
+export const languages: Language[] = [
+  {
+    name: "Arabic (Modern Standard) - Telephony",
+    code: "ar-MS_Telephony",
+  },
+  {
+    name: "Chinese (Mandarin) - Telephony",
+    code: "zh-CN_Telephony",
+  },
+  {
+    name: "Czech  - Telephony",
+    code: "cs-CZ_Telephony",
+  },
+  {
+    name: "Dutch (Belgian) - Telephony",
+    code: "nl-BE_Telephony",
+  },
+  {
+    name: "Dutch (Netherlands) - Telephony",
+    code: "nl-NL_Telephony",
+  },
+  {
+    name: "English (all supported dialects) - Telephony",
+    code: "en-WW_Medical_Telephony",
+  },
+  {
+    name: "English (Australian) - Telephony",
+    code: "en-AU_Telephony",
+  },
+  {
+    name: "English (Australian) - Multimedia",
+    code: "en-AU_Multimedia",
+  },
+  {
+    name: "English (Indian) - Telephony",
+    code: "en-IN_Telephony",
+  },
+  {
+    name: "English (United Kingdom) - Telephony",
+    code: "en-GB_Telephony",
+  },
+  {
+    name: "English (United Kingdom)  - Multimedia",
+    code: "en-GB_Multimedia",
+  },
+  {
+    name: "English (United States) - Telephony",
+    code: "en-US_Telephony",
+  },
+  {
+    name: "English (United States)  - Multimedia",
+    code: "en-US_Multimedia",
+  },
+  {
+    name: "French (Canadian) - Telephony",
+    code: "fr-CA_Telephony",
+  },
+  {
+    name: "French (Canadian)  - Multimedia",
+    code: "fr-CA_Multimedia",
+  },
+  {
+    name: "French (France) - Telephony",
+    code: "fr-FR_Telephony",
+  },
+  {
+    name: "French (France)  - Multimedia",
+    code: "fr-FR_Multimedia",
+  },
+  {
+    name: "German",
+    code: "de-DE_Telephony - Telephony",
+  },
+  {
+    name: "German  - Multimedia",
+    code: "de-DE_Multimedia",
+  },
+  {
+    name: "Hindi (Indian) - Telephony",
+    code: "hi-IN_Telephony",
+  },
+  {
+    name: "Italian - Telephony",
+    code: "it-IT_Telephony",
+  },
+  {
+    name: "Italian  - Multimedia",
+    code: "it-IT_Multimedia",
+  },
+  {
+    name: "Japanese  - Multimedia",
+    code: "ja-JP_Multimedia",
+  },
+  {
+    name: "Korean - Telephony",
+    code: "ko-KR_Telephony",
+  },
+  {
+    name: "Korean  - Multimedia",
+    code: "ko-KR_Multimedia",
+  },
+  {
+    name: "Portuguese (Brazilian) - Telephony",
+    code: "pt-BR_Telephony",
+  },
+  {
+    name: "Portuguese (Brazilian)",
+    code: "pt-BR_Multimedia",
+  },
+  {
+    name: "Spanish (Mexican) - Telephony",
+    code: "es-LA_Telephony",
+  },
+  {
+    name: "Spanish (Castilian) - Telephony",
+    code: "es-ES_Telephony",
+  },
+  {
+    name: "Spanish (Castilian) - Multimedia",
+    code: "es-ES_Multimedia",
+  },
+  {
+    name: "Swedish  - Telephony",
+    code: "sv-SE_Telephony",
+  },
+];
+
+export default languages;
