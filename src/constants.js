4509c52f678aa25aaacd1cd83573889fd0483b25
Dave Horton <daveh@beachdognet.com>
2022-12-05 09:07:13 -0500

    Bugfix/account limits show (#161)
    * fix for APP_ENABLE_ACCOUNT_LIMITS_ALL
    
    * further fix
    
    * remove log statements

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const { REACT_APP_API_BASE_URL, REACT_APP_ENABLE_ACCOUNT_LIMITS_ALL } = process.env;
 export const APP_API_BASE_URL = (window.JAMBONZ) ? window.JAMBONZ.APP_API_BASE_URL : REACT_APP_API_BASE_URL;
+export const APP_ENABLE_ACCOUNT_LIMITS_ALL = (window.JAMBONZ) ? window.JAMBONZ.APP_ENABLE_ACCOUNT_LIMITS_ALL : JSON.parse(REACT_APP_ENABLE_ACCOUNT_LIMITS_ALL);
 export const LIMITS = [
     // {
     //   label: "Max registered devices (0=unlimited)",
@@ -14,8 +15,7 @@ export const LIMITS = [
       category: "voice_call_session",
     }
   ];
-
-  if (REACT_APP_ENABLE_ACCOUNT_LIMITS_ALL) {
+  if (APP_ENABLE_ACCOUNT_LIMITS_ALL || APP_ENABLE_ACCOUNT_LIMITS_ALL === "true") {
     LIMITS.push({
       label: "Licensed calls",
       category: "voice_call_session_license",
