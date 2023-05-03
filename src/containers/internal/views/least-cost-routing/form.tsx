ada92c90de3f79b8d42f4a60eeca74c2a6d098b6
Quan HL <quan.luuhoang8@gmail.com>
2023-05-03 19:50:26 +0700

    wip: implement drag and drop

--- src/containers/internal/views/least-cost-routing/form.tsx
+++ src/containers/internal/views/least-cost-routing/form.tsx
@@ -8,7 +8,7 @@ import {
   useDispatch,
   useSelectState,
 } from "src/store";
-import { MSG_REQUIRED_FIELDS } from "src/constants";
+import { MSG_REQUIRED_FIELDS, NOT_AVAILABLE_PREFIX } from "src/constants";
 import { setLocation } from "src/store/localStore";
 import { AccountSelect, Message, Selector } from "src/components/forms";
 import type {
@@ -22,7 +22,6 @@ import type {
 import { ROUTE_INTERNAL_LEST_COST_ROUTING } from "src/router/routes";
 import {
   deleteLcr,
-  deleteLcrRoute,
   postLcrCarrierSetEntry,
   putLcrCarrierSetEntries,
   putLcrRoutes,
@@ -32,11 +31,14 @@ import {
   getLcrRoute,
 } from "src/api";
 import { USER_ACCOUNT, USER_ADMIN } from "src/api/constants";
-import { hasLength } from "src/utils";
 import { postLcr } from "src/api";
 import { postLcrRoute } from "src/api";
 import DeleteLcr from "./delete";
 import { Scope } from "src/store/types";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Container from "./container";
+import { v4 } from "uuid";
 
 type LcrFormProps = {
   lcrDataMap?: UseApiDataMap<Lcr>;
@@ -44,8 +46,8 @@ type LcrFormProps = {
 };
 
 export const LcrForm = ({ lcrDataMap, lcrRouteDataMap }: LcrFormProps) => {
-  const MAX_ROUTES = 5;
   const LCR_ROUTE_TEMPLATE: LcrRoute = {
+    lcr_route_sid: `${NOT_AVAILABLE_PREFIX}${v4()}`,
     regex: "",
     lcr_sid: "",
     priority: 0,
@@ -128,14 +130,6 @@ export const LcrForm = ({ lcrDataMap, lcrRouteDataMap }: LcrFormProps) => {
     return ret;
   }, [accountSid, carriers]);
 
-  const sortSetLcrRoutes = (routes: LcrRoute[]) => {
-    let sorted = [...routes];
-    sorted = sorted.sort((p1, p2) =>
-      p1.priority > p2.priority ? 1 : p1.priority < p2.priority ? -1 : 0
-    );
-    return sorted;
-  };
-
   if (lcrDataMap && lcrDataMap.data && lcrDataMap.data !== previouseLcr) {
     setLcrName(lcrDataMap.data.name || "");
     setIsActive(lcrDataMap.data.is_active);
@@ -183,60 +177,6 @@ export const LcrForm = ({ lcrDataMap, lcrRouteDataMap }: LcrFormProps) => {
       },
     ];
     setLcrRoutes(ls);
-  };
-
-  const updateLcrRoute = (
-    index: number,
-    key: string,
-    value: typeof lcrRoutes[number][keyof LcrRoute]
-  ) => {
-    setLcrRoutes(
-      lcrRoutes.map((lr, i) => (i === index ? { ...lr, [key]: value } : lr))
-    );
-  };
-
-  const updateLcrCarrierSetEntries = (
-    index1: number,
-    index2: number,
-    key: string,
-    value: unknown
-  ) => {
-    setLcrRoutes(
-      lcrRoutes.map((lr, i) =>
-        i === index1
-          ? {
-              ...lr,
-              lcr_carrier_set_entries: lr.lcr_carrier_set_entries?.map(
-                (entry, j) =>
-                  j === index2
-                    ? {
-                        ...entry,
-                        [key]: value,
-                      }
-                    : entry
-              ),
-            }
-          : lr
-      )
-    );
-  };
-
-  const moveLcrRoute = (from: number, to: number) => {
-    lcrRoutes[from].priority = to;
-    lcrRoutes[to].priority = from;
-    setLcrRoutes(sortSetLcrRoutes(lcrRoutes));
-  };
-
-  const handleRouteDelete = (r: LcrRoute | undefined) => {
-    if (r && r.lcr_route_sid) {
-      deleteLcrRoute(r.lcr_route_sid)
-        .then(() => {
-          toastSuccess("Least cost routing rule successfully deleted");
-        })
-        .catch((error) => {
-          toastError(error);
-        });
-    }
   };
 
   const getLcrPayload = (): Lcr => {
@@ -375,7 +315,10 @@ export const LcrForm = ({ lcrDataMap, lcrRouteDataMap }: LcrFormProps) => {
       putLcr(lcrDataMap.data.lcr_sid, lcrPayload).then(() => {
         Promise.all(
           lcrRoutes.map((route, i) => {
-            if (route.lcr_route_sid) {
+            if (
+              route.lcr_route_sid &&
+              !route.lcr_route_sid?.startsWith(NOT_AVAILABLE_PREFIX)
+            ) {
               handleLcrRoutePut(
                 lcrDataMap.data?.lcr_sid || "",
                 route.lcr_route_sid,
@@ -568,94 +511,26 @@ export const LcrForm = ({ lcrDataMap, lcrRouteDataMap }: LcrFormProps) => {
               Route based on first match<span>*</span>
             </label>
             <label htmlFor="sip_gateways">Digit pattern / Carrier</label>
-            {hasLength(lcrRoutes) &&
-              lcrRoutes.map((lr, i) => (
-                <div key={`lcr_route_${i}`} className="lcr lcr--route">
-                  <div>
-                    <div>
-                      <input
-                        id={`lcr_route_regex_${i}`}
-                        name={`lcr_route_regex_${i}`}
-                        type="text"
-                        placeholder="Digit prefix or regex"
-                        required
-                        value={lr.regex || ""}
-                        onChange={(e) => {
-                          updateLcrRoute(i, "regex", e.target.value);
-                        }}
-                      />
-                    </div>
-                    <div>
-                      <Selector
-                        id={`lcr_carrier_set_entry_carrier_${i}`}
-                        name={`lcr_carrier_set_entry_carrier_${i}`}
-                        placeholder="Carrier"
-                        value={
-                          lr.lcr_carrier_set_entries &&
-                          lr.lcr_carrier_set_entries.length > 0
-                            ? lr.lcr_carrier_set_entries[0].voip_carrier_sid
-                              ? lr.lcr_carrier_set_entries[0].voip_carrier_sid
-                              : ""
-                            : ""
-                        }
-                        required
-                        options={carrierSelectorOptions}
-                        onChange={(e) => {
-                          updateLcrCarrierSetEntries(
-                            i,
-                            0,
-                            "voip_carrier_sid",
-                            e.target.value
-                          );
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <button
-                    className="btnty btn__delete"
-                    title="Delete route"
-                    type="button"
-                    onClick={() => {
-                      handleRouteDelete(lcrRoutes.find((g2, i2) => i2 === i));
-                      setLcrRoutes(lcrRoutes.filter((g2, i2) => i2 !== i));
-                    }}
-                  >
-                    <Icon>
-                      <Icons.Trash2 />
-                    </Icon>
-                  </button>
-                  {i !== 0 && (
-                    <button
-                      className="btnty btn__up_level"
-                      title="Move route up"
-                      type="button"
-                      onClick={() => {
-                        moveLcrRoute(i, i - 1);
-                      }}
-                    >
-                      <Icon subStyle="teal">
-                        <Icons.ArrowUp />
-                      </Icon>
-                    </button>
-                  )}
-                </div>
-              ))}
-            {lcrRoutes.length < MAX_ROUTES && (
-              <ButtonGroup left>
-                <button
-                  className="btnty"
-                  type="button"
-                  title="Add route"
-                  onClick={() => {
-                    addLcrRoutes();
-                  }}
-                >
-                  <Icon subStyle="teal">
-                    <Icons.Plus />
-                  </Icon>
-                </button>
-              </ButtonGroup>
-            )}
+            <DndProvider backend={HTML5Backend}>
+              <Container
+                lcrRoute={[lcrRoutes, setLcrRoutes]}
+                carrierSelectorOptions={carrierSelectorOptions}
+              />
+            </DndProvider>
+            <ButtonGroup left>
+              <button
+                className="btnty"
+                type="button"
+                title="Add route"
+                onClick={() => {
+                  addLcrRoutes();
+                }}
+              >
+                <Icon subStyle="teal">
+                  <Icons.Plus />
+                </Icon>
+              </button>
+            </ButtonGroup>
           </fieldset>
           <fieldset>
             <div className="grid grid--col3">
