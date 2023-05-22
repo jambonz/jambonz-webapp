b5bc6265e0cdbe9f3a621682585f3da62bfd555c
Dave Horton <daveh@beachdognet.com>
2023-05-22 14:32:12 -0400

    disabling lcr, jaeger, and custom speech for k8s

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,21 @@ API_PORT="${API_PORT:-3000}"
 API_VERSION="${API_VERSION:-v1}"
 API_BASE_URL=${API_BASE_URL:-http://$PUBLIC_IPV4:$API_PORT/$API_VERSION}
 
+# Default to "false" if not set
+DISABLE_LCR=${DISABLE_LCR:-false}
+DISABLE_JAEGER_TRACING=${DISABLE_JAEGER_TRACING:-false}
+DISABLE_CUSTOM_SPEECH=${DISABLE_CUSTOM_SPEECH:-false}
+ENABLE_FORGOT_PASSWORD=${ENABLE_FORGOT_PASSWORD:-false}
+
 # Serialize window global to provide the API URL to static frontend dist
 # This is declared and utilized in the web app: src/api/constants.ts
-SCRIPT_TAG="<script>window.JAMBONZ = { API_BASE_URL: \"${API_BASE_URL}\" };</script>"
+SCRIPT_TAG="<script>window.JAMBONZ = {
+    API_BASE_URL: \"${API_BASE_URL}\",
+    DISABLE_LCR: \"${DISABLE_LCR}\",
+    DISABLE_JAEGER_TRACING: \"${DISABLE_JAEGER_TRACING}\",
+    DISABLE_CUSTOM_SPEECH: \"${DISABLE_CUSTOM_SPEECH}\",
+    ENABLE_FORGOT_PASSWORD: \"${ENABLE_FORGOT_PASSWORD}\"
+  };</script>"
 sed -i -e "\@</head>@i\    $SCRIPT_TAG" ./dist/index.html
 
 # Start the frontend web app static server
