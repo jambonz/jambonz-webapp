c78a37e03119e227f906f29883df384c4bc32693
Dave Horton <daveh@beachdognet.com>
2023-05-22 14:51:22 -0400

    fix syntax error in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,13 +17,7 @@ ENABLE_FORGOT_PASSWORD=${ENABLE_FORGOT_PASSWORD:-false}
 
 # Serialize window global to provide the API URL to static frontend dist
 # This is declared and utilized in the web app: src/api/constants.ts
-SCRIPT_TAG="<script>window.JAMBONZ = {
-    API_BASE_URL: \"${API_BASE_URL}\",
-    DISABLE_LCR: \"${DISABLE_LCR}\",
-    DISABLE_JAEGER_TRACING: \"${DISABLE_JAEGER_TRACING}\",
-    DISABLE_CUSTOM_SPEECH: \"${DISABLE_CUSTOM_SPEECH}\",
-    ENABLE_FORGOT_PASSWORD: \"${ENABLE_FORGOT_PASSWORD}\"
-  };</script>"
+SCRIPT_TAG="<script>window.JAMBONZ = {API_BASE_URL: \"${API_BASE_URL}\",DISABLE_LCR: \"${DISABLE_LCR}\",DISABLE_JAEGER_TRACING: \"${DISABLE_JAEGER_TRACING}\",DISABLE_CUSTOM_SPEECH: \"${DISABLE_CUSTOM_SPEECH}\",ENABLE_FORGOT_PASSWORD: \"${ENABLE_FORGOT_PASSWORD}\"};</script>"
 sed -i -e "\@</head>@i\    $SCRIPT_TAG" ./dist/index.html
 
 # Start the frontend web app static server
