4a2c36ebba9b0ffc220cffc12ea1587ec3047e84
Hoan Luu Huu <110280845+xquanluu@users.noreply.github.com>
2023-09-19 07:15:53 +0700

    allow sip port is null (#323)
    * allow sip port is null
    
    * fix port placeholder when protocol is tls or tls/srtp

--- src/api/types.ts
+++ src/api/types.ts
@@ -459,7 +459,6 @@ export interface PredefinedCarrier extends Carrier {
 export interface Gateway {
   voip_carrier_sid: string;
   ipv4: string;
-  port: number;
   netmask: number;
   inbound: number;
   outbound: number;
@@ -469,6 +468,7 @@ export interface SipGateway extends Gateway {
   sip_gateway_sid?: null | string;
   is_active: boolean;
   protocol?: string;
+  port: number | null;
   pad_crypto?: boolean;
 }
 
@@ -476,6 +476,7 @@ export interface SmppGateway extends Gateway {
   smpp_gateway_sid?: null | string;
   is_primary: boolean;
   use_tls: boolean;
+  port: number;
 }
 
 export interface Lcr {
