b02a6c84b50d3bf3fa80794036529d4eecaba3b4
Quan HL <quan.luuhoang8@gmail.com>
2023-10-12 16:05:34 +0700

    wip

--- src/vendor/index.tsx
+++ src/vendor/index.tsx
@@ -8,7 +8,7 @@ import type {
 } from "./types";
 
 export const LANG_EN_US = "en-US";
-export const ELEVENLABS_LANG_EN = "en-US";
+export const ELEVENLABS_LANG_EN = "en";
 export const LANG_COBALT_EN_US = "en_US-8khz";
 export const LANG_EN_US_STANDARD_C = "en-US-Standard-C";
 export const VENDOR_GOOGLE = "google";
